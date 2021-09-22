9ebca110b475a5f5bfa200b038f3939af12a7798
SoraQu <57914474+Chance20022@users.noreply.github.com>
2021-09-22 21:49:24 +0300

    Работающая анимация

--- Journey to uncharted lands/Journey to uncharted lands.cpp
+++ Journey to uncharted lands/Journey to uncharted lands.cpp
@@ -1,201 +1,85 @@
-﻿#include <iostream>
-#include <SDL.h>
-#include <SDL_image.h>
+﻿#include "Essence.h"
 
-//Texture wrapper class
-class LTexture
-{
-public:
-    //Initializes variables
-    LTexture();
+bool loadMedia();
+void close();
+bool init();
 
-    //Deallocates memory
-    ~LTexture();
-
-    //Loads image at specified path
-    bool loadFromFile(std::string path);
-
-    //Deallocates texture
-    void free();
-
-    //Set color modulation
-    void setColor(Uint8 red, Uint8 green, Uint8 blue);
-
-    //Renders texture at given point
-    void render(int x, int y, SDL_Rect* clip = NULL);
-
-    //Gets image dimensions
-    int getWidth();
-    int getHeight();
-
-private:
-    //The actual hardware texture
-    SDL_Texture* mTexture;
-
-    //Image dimensions
-    int mWidth;
-    int mHeight;
-};
-
-//Screen dimension constants
-const int SCREEN_WIDTH = 1280;
-const int SCREEN_HEIGHT = 720;
-
-//The window we'll be rendering to
-SDL_Window* gWindow = NULL;
-
-//The window renderer
-SDL_Renderer* gRenderer = NULL;
-
-//Scene textures
-LTexture gFooTexture;
-LTexture gBackgroundTexture;
-
-//Scene sprites
-SDL_Rect gSpriteClips[4];
+//Walking animation
+const int WALKING_ANIMATION_FRAMES = 20;
+SDL_Rect gSpriteClips[WALKING_ANIMATION_FRAMES];
 LTexture gSpriteSheetTexture;
 
-LTexture gModulatedTexture;
-
-void LTexture::setColor(Uint8 red, Uint8 green, Uint8 blue)
+int main(int argc, char* args[])
 {
-    //Modulate texture
-    SDL_SetTextureColorMod(mTexture, red, green, blue);
-}
+    //Main loop flag
+    bool quit = false;
 
-LTexture::LTexture()
-{
-    //Initialize
-    mTexture = NULL;
-    mWidth = 0;
-    mHeight = 0;
-}
+    //Event handler
+    SDL_Event e;
 
-LTexture::~LTexture()
-{
-    //Deallocate
-    free();
-}
+    //Current animation frame
+    int frame = 0;
 
-bool LTexture::loadFromFile(std::string path)
-{
-    //Get rid of preexisting texture
-    free();
-
-    //The final texture
-    SDL_Texture* newTexture = NULL;
-
-    //Load image at specified path
-    SDL_Surface* loadedSurface = IMG_Load(path.c_str());
-    if (loadedSurface == NULL)
-    {
-        printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
+    if (!init()) {
+        printf("Error init() \n");
     }
-    else
+
+    if (!loadMedia()) printf("Error loadMedia() \n");
+
+    //While application is running
+    while (!quit)
     {
-        //Color key image
-        SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0, 0xFF, 0xFF));
-        //Create texture from surface pixels
-        newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
-        if (newTexture == NULL)
+        //Handle events on queue
+        while (SDL_PollEvent(&e) != 0)
         {
-            printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
-        }
-        else
-        {
-            //Get image dimensions
-            mWidth = loadedSurface->w;
-            mHeight = loadedSurface->h;
+            //User requests quit
+            if (e.type == SDL_QUIT)
+            {
+                quit = true;
+            }
         }
 
-        //Get rid of old loaded surface
-        SDL_FreeSurface(loadedSurface);
+        SDL_RenderClear(gRenderer);
+        //Render current frame
+        SDL_Rect* currentClip = &gSpriteClips[frame];
+        gSpriteSheetTexture.render((SCREEN_WIDTH - currentClip->w) / 2, (SCREEN_HEIGHT - currentClip->h) / 2, currentClip);
+
+        //Update screen
+        SDL_RenderPresent(gRenderer);
+
+        //Go to next frame
+        frame++;
+
+        //Cycle animation
+        if (frame >= WALKING_ANIMATION_FRAMES)
+        {
+            frame = 0;
+        }
     }
+    close();
 
-    //Return success
-    mTexture = newTexture;
-    return mTexture != NULL;
-}
-
-void LTexture::free()
-{
-    //Free texture if it exists
-    if (mTexture != NULL)
-    {
-        SDL_DestroyTexture(mTexture);
-        mTexture = NULL;
-        mWidth = 0;
-        mHeight = 0;
-    }
-}
-
-void LTexture::render(int x, int y, SDL_Rect* clip)
-{
-    //Set rendering space and render to screen
-    SDL_Rect renderQuad = { x, y, mWidth, mHeight };
-
-    //Set clip rendering dimensions
-    if (clip != NULL)
-    {
-        renderQuad.w = clip->w;
-        renderQuad.h = clip->h;
-    }
-
-    //Render to screen
-    SDL_RenderCopy(gRenderer, mTexture, clip, &renderQuad);
-}
-
-int LTexture::getWidth()
-{
-    return mWidth;
-}
-
-int LTexture::getHeight()
-{
-    return mHeight;
+    return 0;
 }
 
 bool loadMedia()
 {
     //Loading success flag
     bool success = true;
-
-    //Load sprite sheet texture
-    if (!gSpriteSheetTexture.loadFromFile("Image/dots.png"))
-    {
-        printf("Failed to load sprite sheet texture!\n");
+    int x = 0;
+    if (gSpriteSheetTexture.loadFromFile("Image/Walk.png")) {
+        printf("Failed to load walking animation texture!\n");
         success = false;
     }
-    else
-    {
-        //Set top left sprite
-        gSpriteClips[0].x = 0;
-        gSpriteClips[0].y = 0;
-        gSpriteClips[0].w = 100;
-        gSpriteClips[0].h = 100;
+    else {
+        for (int i = 0; i < 20; i++) {//109 - ширина, 159,5 - высота
+            //Set sprite clips
+            gSpriteClips[i].x = x;
+            gSpriteClips[i].y = 0;
+            gSpriteClips[i].w = 109;
+            gSpriteClips[i].h = 158;
 
-        //Set top right sprite
-        gSpriteClips[1].x = 100;
-        gSpriteClips[1].y = 0;
-        gSpriteClips[1].w = 100;
-        gSpriteClips[1].h = 100;
-
-        //Set bottom left sprite
-        gSpriteClips[2].x = 0;
-        gSpriteClips[2].y = 100;
-        gSpriteClips[2].w = 100;
-        gSpriteClips[2].h = 100;
-
-        //Set bottom right sprite
-        gSpriteClips[3].x = 100;
-        gSpriteClips[3].y = 100;
-        gSpriteClips[3].w = 100;
-        gSpriteClips[3].h = 100;
-    }
-
-    if (!gModulatedTexture.loadFromFile("Image/colors.png")) {
-        printf("Failed to load sprite sheet texture!\n");
-        success = false;
+            x += 109;
+        }
     }
 
     return success;
@@ -203,9 +87,7 @@ bool loadMedia()
 
 void close()
 {
-    //Free loaded images
-    gFooTexture.free();
-    gBackgroundTexture.free();
+    gSpriteSheetTexture.free();
 
     //Destroy window    
     SDL_DestroyRenderer(gRenderer);
@@ -240,8 +122,8 @@ bool init()
         }
         else
         {
-            //Create renderer for window
-            gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+            //Create vsynced renderer for window
+            gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
             if (gRenderer == NULL)
             {
                 printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
@@ -264,87 +146,4 @@ bool init()
     }
 
     return success;
-}
-
-int main(int argc, char* args[])
-{
-    //Main loop flag
-    bool quit = false;
-
-    //Event handler
-    SDL_Event e;
-
-    //Modulation components
-    Uint8 r = 255;
-    Uint8 g = 255;
-    Uint8 b = 255;
-
-    if (!init()) {
-        printf("Error init() \n");
-    }
-
-    if (!loadMedia()) printf("Error loadMedia() \n");
-
-    //While application is running
-    while (!quit)
-    {
-        //Handle events on queue
-        while (SDL_PollEvent(&e) != 0)
-        {
-            //User requests quit
-            if (e.type == SDL_QUIT)
-            {
-                quit = true;
-            }
-            //On keypress change rgb values
-            else if (e.type == SDL_KEYDOWN)
-            {
-                switch (e.key.keysym.sym)
-                {
-                    //Increase red
-                case SDLK_q:
-                    r += 32;
-                    break;
-
-                    //Increase green
-                case SDLK_w:
-                    g += 32;
-                    break;
-
-                    //Increase blue
-                case SDLK_e:
-                    b += 32;
-                    break;
-
-                    //Decrease red
-                case SDLK_a:
-                    r -= 32;
-                    break;
-
-                    //Decrease green
-                case SDLK_s:
-                    g -= 32;
-                    break;
-
-                    //Decrease blue
-                case SDLK_d:
-                    b -= 32;
-                    break;
-                }
-            }
-        }
-        //Clear screen
-        SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-        SDL_RenderClear(gRenderer);
-
-        //Modulate and render texture
-        gModulatedTexture.setColor(r, g, b);
-        gModulatedTexture.render(0, 0);
-
-        //Update screen
-        SDL_RenderPresent(gRenderer);
-    }
-    close();
-
-    return 0;
 }
